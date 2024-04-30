472d756286fb2f8b3c8efa2bbef2552fbc4fa39a
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-30 17:59:46 -0300

    Using the selector in the Topic.js file to render the topics

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -1,33 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link, useParams, Navigate } from "react-router-dom";
-import ROUTES from "../../app/routes";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useParams, Navigate } from 'react-router-dom';
+import ROUTES from '../../app/routes';
 // import selectors
+import { selectTopics } from './topicsSlice';
 
 export default function Topic() {
-  const topics = {};  // replace with selector
+  const topics = useSelector(selectTopics); // replace with selector
   const quizzes = {}; // replace with selector
   const { topicId } = useParams();
   const topic = topics[topicId];
 
-  if(!topic) {
-    return <Navigate to={ROUTES.topicsRoute()} replace/>
+  if (!topic) {
+    return <Navigate to={ROUTES.topicsRoute()} replace />;
   }
-  
+
   const quizzesForTopic = topic.quizIds.map((quizId) => quizzes[quizId]);
 
   return (
     <section>
-      <img src={topic.icon} alt="" className="topic-icon" />
+      <img src={topic.icon} alt='' className='topic-icon' />
       <h1>{topic.name}</h1>
-      <ul className="quizzes-list">
+      <ul className='quizzes-list'>
         {quizzesForTopic.map((quiz) => (
-          <li className="quiz" key={quiz.id}>
+          <li className='quiz' key={quiz.id}>
             <Link to={ROUTES.quizRoute(quiz.id)}>{quiz.name}</Link>
           </li>
         ))}
       </ul>
-      <Link to="/quizzes/new" className="button center">
+      <Link to='/quizzes/new' className='button center'>
         Create a New Quiz
       </Link>
     </section>
